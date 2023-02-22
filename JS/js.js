38c8543df197dc46b1de8749ab1895f5b79fdeac
Yanina M Fernandez <fernandezyaninam@gmail.com>
2023-02-22 16:04:26 -0300

    Proyecto final V1

---
+++ JS/js.js
@@ -0,0 +1,11 @@
+/*
+document.getElementById('boton').onclick = function(){
+    window.alert('Muchas gracias por su interes');
+    window.close()
+}
+*/
+document.getElementById('boton').addEventListener('click',function(){
+    window.alert('Muchas gracias por su interés. A disposición. Saludos cordiales!');
+    window.close();
+}
+);
